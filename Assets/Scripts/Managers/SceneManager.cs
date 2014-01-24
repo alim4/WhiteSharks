68f159fe8ddb3e58e24bf656777aa876da283b9c
alim4 <theineffablebob@gmail.com>
2014-01-24 01:30:59 -0800

    Reformatted SceneManager to fit coding conventions

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -51,21 +51,15 @@ namespace Assets.Scenes.Scripts
 		/// <summary>
 		/// Gets the names of all levels in the game.
 		/// </summary>
-		public static string[] Levels
-		{
-			get
-			{
-				if (_levels == null)
-				{
+		public static string[] Levels {
+			get {
+				if (_levels == null) {
 					string directory;
 					
 					// The directory depends on the environment. In the editor, relative paths can be used.
-					if (Application.isEditor)
-					{
+					if (Application.isEditor) {
 						directory = EDITOR_LEVELS_FILE_DIRECTORY;
-					}
-					else
-					{
+					} else {
 						string dataPath = Application.dataPath;
 						
 						Debug.Log( string.Format("Data path detected at '{0}'.", dataPath) );
@@ -89,8 +83,7 @@ namespace Assets.Scenes.Scripts
 		/// <param name="target"></param>
 		/// <param name="pathToBuiltProject"></param>
 		[UnityEditor.Callbacks.PostProcessBuild]
-		public static void PostProcessBuild(UnityEditor.BuildTarget target, string pathToBuiltProject)
-		{
+		public static void PostProcessBuild(UnityEditor.BuildTarget target, string pathToBuiltProject) {
 			const string DATA_FOLDER = "{0}_Data";
 			
 			Debug.Log(string.Format("Post-processing build '{0}' at '{1}'.", target, pathToBuiltProject));
@@ -114,23 +107,18 @@ namespace Assets.Scenes.Scripts
 		/// method updates the levels file, if applicable.
 		/// </summary>
 		[UnityEditor.Callbacks.PostProcessScene]
-		public static void PostProcessScene()
-		{
+		public static void PostProcessScene() {
 			Debug.Log( "Post-processing scene." );
 			
-			if ( !_hasUpdatedLevelsFile )
-			{
+			if (!_hasUpdatedLevelsFile) {
 				// Only write a levels file if we're in the editor. If not, the PostProcessBuild method will do this, because the PostProcessScene is called for all scenes.
-				if ( Application.isEditor )
-				{
+				if (Application.isEditor) {
 					Debug.Log( string.Format( "Detected editor, writing levels file to '{0}'.", EDITOR_LEVELS_FILE_DIRECTORY ) );
 					
 					WriteLevelsFile( EDITOR_LEVELS_FILE_DIRECTORY );
 					_hasUpdatedLevelsFile = true;
 				}
-			}
-			else
-			{
+			} else {
 				Debug.Log( "Already updated levels file." );
 			}
 			
@@ -141,15 +129,12 @@ namespace Assets.Scenes.Scripts
 		/// Writes or creates the levels file by collecting all levels configured in the build and (re-)writing the levels file at the provided directory.
 		/// </summary>
 		/// <param name="directory">The directory to write the levels file to.</param>
-		private static void WriteLevelsFile(string directory)
-		{
+		private static void WriteLevelsFile(string directory) {
 			List<string> levelNames = new List<string>();
 			
 			// Collect the names of all levels in the build settings.
-			foreach (UnityEditor.EditorBuildSettingsScene buildSettingsScene in UnityEditor.EditorBuildSettings.scenes)
-			{
-				if (buildSettingsScene.enabled)
-				{
+			foreach (UnityEditor.EditorBuildSettingsScene buildSettingsScene in UnityEditor.EditorBuildSettings.scenes) {
+				if (buildSettingsScene.enabled) {
 					string name = buildSettingsScene.path.Substring(buildSettingsScene.path.LastIndexOf(Path.AltDirectorySeparatorChar) + 1);
 					name = name.Substring(0, name.Length - 6);
 					
@@ -164,12 +149,9 @@ namespace Assets.Scenes.Scripts
 			Debug.Log( string.Format("Writing levels file to '{0}'.", path) );
 			
 			// Write the names of all levels to a file, so that it can be retrieved when running.
-			using (FileStream stream = File.Open(path, FileMode.Create, FileAccess.Write))
-			{
-				using (StreamWriter writer = new StreamWriter(stream))
-				{
-					foreach (string levelName in levelNames)
-					{
+			using (FileStream stream = File.Open(path, FileMode.Create, FileAccess.Write)) {
+				using (StreamWriter writer = new StreamWriter(stream)) {
+					foreach (string levelName in levelNames) {
 						writer.WriteLine( levelName );
 					}
 				}
@@ -182,31 +164,24 @@ namespace Assets.Scenes.Scripts
 		/// </summary>
 		/// <param name="directory">The directory that contains the levels file.</param>
 		/// <returns>The discovered levels.</returns>
-		private static string[] ReadLevelsFile(string directory)
-		{
+		private static string[] ReadLevelsFile(string directory) {
 			string path = Path.Combine(directory, LEVEL_FILE_NAME);
 			
 			Debug.Log(string.Format("Reading levels file from '{0}'.", path));
 			
 			List<string> levelNames = new List<string>();
 			
-			if (File.Exists(path))
-			{
+			if (File.Exists(path)) {
 				// Read the names of all levels from the levels file.
-				using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read))
-				{
-					using (StreamReader reader = new StreamReader(stream))
-					{
+				using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read)) {
+					using (StreamReader reader = new StreamReader(stream)) {
 						// Possibly use ReadToEnd and string.Split(fileContent, Environment.NewLine).
-						while (!reader.EndOfStream)
-						{
+						while (!reader.EndOfStream) {
 							levelNames.Add(reader.ReadLine());
 						}
 					}
 				}
-			}
-			else
-			{
+			} else {
 				Debug.LogWarning("Levels file does not exist, no level names available at run-time.");
 			}
 			
