94a5e1b9c5c78ccddb25d5f0c8c48e77c09d48d1
alim4 <theineffablebob@gmail.com>
2014-02-15 20:09:45 -0800

    Add Dialoguer
    Add

---
+++ Assets/Scripts/DialogueGUI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueGUI : MonoBehaviour {
+
+	private bool _showing;
+
+	private string _text;
+	private string[] _choices;
+	
+	// Use this for initialization
+	void Start () {
+		Dialoguer.events.onStarted += onStarted;
+		Dialoguer.events.onEnded += onEnded;
+		Dialoguer.events.onTextPhase += onTextPhase;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if (!_showing)
+			return;
+
+		GUI.Box (new Rect (30, 10, 300, 180), _text);
+
+		if (_choices == null)
+		{
+			if (GUI.Button (new Rect(30, 220, 200, 30), "Continue"))
+			{
+				Dialoguer.ContinueDialogue();
+			}
+		}
+		else
+		{
+			for (int i = 0; i < _choices.Length; ++i)
+			{
+				if (GUI.Button (new Rect(30, 220 + (40*i), 200, 30), _choices[i]))
+				{
+					Dialoguer.ContinueDialogue(i);
+				}
+			}
+		}
+	}
+
+	private void onStarted()
+	{
+		_showing = true;
+	}
+
+	private void onEnded()
+	{
+		_showing = false;
+	}
+
+	private void onTextPhase(DialoguerTextData data)
+	{
+		_text = data.text;
+		_choices = data.choices;
+	}
+}
