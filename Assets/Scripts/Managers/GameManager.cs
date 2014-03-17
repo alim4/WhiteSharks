fd1ecf564cfe563370344983eeb14a59ad0092e1
alim4 <theineffablebob@gmail.com>
2014-03-17 00:46:01 -0700

    Update Merge

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,15 +8,16 @@ using System;
 
 public class GameManager : MonoBehaviour {
 	// Declare properties
-
-
+	
+	
 	//Variables for case control
 	public static int currentEpisode = 0;
 	public static string[] episodeStartLevels = new String[12]{"JaneIntro","","","","","","","","","","",""};
 	public static string[] episodeDialogues = new string[12]{"case0","","","","","","","","","","",""};
-
+	
 	public static bool dialogueJustFinished = false;
-
+	public static bool firstTimeOffice = true;
+	
 	public bool playerInScene = false;
 	private static GameManager instance;
 	private gameStates _currentState;
@@ -36,20 +37,20 @@ public class GameManager : MonoBehaviour {
 	public static NPC guilty;
 	public static CaseObject weapon;
 	public static string room;
-
+	
 	//This is the target state the player wishes to reach for maximum score
 	public static Dictionary idealGameState;
-
+	
 	//Shammy 0, Noel 1, Carlos 2
 	public static int[] npcConversations = new int[7]{0, 0, 0, 0, 0, 0, 0};
-
+	
 	//Handles mouse cursor information
 	public static int cursorSize = 32;
 	public static List<Texture2D> mouseSprites;
 	public static string[] spriteIndex;
 	public static Texture2D currMouse;
-
-
+	
+	
 	public static GameManager Instance {
 		get {
 			if (instance == null) {
@@ -59,7 +60,7 @@ public class GameManager : MonoBehaviour {
 			return instance;
 		}
 	}
-
+	
 	// Sets instance to null when the application quits
 	public void OnApplicationQuit() {
 		instance = null;
@@ -89,10 +90,10 @@ public class GameManager : MonoBehaviour {
 		return currentMainCharacter;
 	}
 	// get player script of main character
- 	public playerScript getPlayerScript(){
- 		return (playerScript)GameObject.Find (this.GetMainCharacter() + "(Clone)").GetComponent<playerScript> ();
- 	}
-
+	public playerScript getPlayerScript(){
+		return (playerScript)GameObject.Find (this.GetMainCharacter() + "(Clone)").GetComponent<playerScript> ();
+	}
+	
 	/// <summary>
 	/// Starts the game state and sets initial values
 	/// Should be called during gameStart
@@ -101,17 +102,17 @@ public class GameManager : MonoBehaviour {
 		Debug.Log("Creating a new game state");
 		//CaseGenerator c = new CaseGenerator("","");
 		//GameManager.setNPCS = 
-
+		
 		// Set default properties
 		_currLevel = "Level 1";
 		_name = "My Character";
 		_currentState = gameStates.INGAME;
 		idealGameState = new Dictionary ();
-
+		
 		// Load character select screen
 		Application.LoadLevel ("CharacterSele");
 	}
-
+	
 	/// <summary>
 	/// Generates the case
 	/// </summary>
@@ -121,7 +122,7 @@ public class GameManager : MonoBehaviour {
 		//Debug.Log ("the case in GM " + guilty + " " + weapon + " " + room);
 		//Debug.Log (theCase.getRoom());
 	}
-
+	
 	/// <summary>
 	/// Calculates the score
 	/// </summary>
@@ -130,7 +131,7 @@ public class GameManager : MonoBehaviour {
 		// calcs score
 		return 0;
 	}
-
+	
 	/// <summary>
 	/// Draws the score
 	/// </summary>
@@ -145,7 +146,7 @@ public class GameManager : MonoBehaviour {
 		Debug.Log("Qutting the game");
 		Application.Quit ();
 	}
-
+	
 	/// <summary>
 	/// Get the current level
 	/// </summary>
@@ -156,7 +157,7 @@ public class GameManager : MonoBehaviour {
 		else
 			return "currLevel is null!";
 	}
-
+	
 	/// <summary>
 	/// Set the currLevel
 	/// </summary>
@@ -164,7 +165,7 @@ public class GameManager : MonoBehaviour {
 	public void setLevel(string level) {
 		_currLevel = level;
 	}
-
+	
 	/// <summary>
 	/// Gets the name
 	/// </summary>
@@ -175,7 +176,7 @@ public class GameManager : MonoBehaviour {
 		else
 			return "name is null!";
 	}
-
+	
 	/// <summary>
 	/// Sets the name
 	/// </summary>
@@ -183,22 +184,22 @@ public class GameManager : MonoBehaviour {
 	public void setName(string newName) {
 		_name = newName;
 	}
-
+	
 	public void setState(gameStates state) {
 		_currentState = state;
 	}
-
+	
 	public void OnGUI() {
 		//GUI.TextArea(new Rect(1, 1, 100, 20), _currentState.ToString());
-
+		
 		//Handle mouse updates here
-
+		
 		GUI.DrawTexture (new Rect (Input.mousePosition.x - cursorSize / 16, (Screen.height - Input.mousePosition.y) - cursorSize / 16,
 		                           cursorSize, cursorSize), currMouse);
-
-
+		
+		
 	}
-
+	
 	//Access function for updating the GameManager's dictionary
 	public void addEntry(DictEntry newEntry){
 		idealGameState.addNewEntry (newEntry);
@@ -213,53 +214,53 @@ public class GameManager : MonoBehaviour {
 	public void printGoal(){
 		idealGameState.printEntries ();
 	}
-
+	
 	//Access function for copying the dictionary
 	public Dictionary getDict(){
 		return idealGameState;
 	}
-
-
+	
+	
 	//Initialize the sprite array for the mouse to draw
 	//Loads in from Sprites/Mouse Icons
 	//Sets the initial icon to Walk
 	void setIcons(){
-
+		
 		mouseSprites = new List<Texture2D>();
 		Screen.showCursor = false;
-
+		
 		foreach (object o in Resources.LoadAll("MouseIcons", typeof(Texture2D))) {
 			mouseSprites.Add(o as Texture2D);
 		}
-
+		
 		spriteIndex = new string[mouseSprites.Count];
 		
 		for(int i=0; i< spriteIndex.Length; i++) {
 			spriteIndex[i] = mouseSprites[i].name;
 		}
-
+		
 		currMouse = (Texture2D) mouseSprites[Array.IndexOf(spriteIndex, "Walk_Icon")];
 	}
-
+	
 	//Updates the current sprite when called by another game object
 	//Takes in a string based on what kind of object it is that signifies the icon the cursor should be
 	public void updateMouseIcon(string whichSprite){
 		currMouse = (Texture2D)mouseSprites [Array.IndexOf (spriteIndex, whichSprite)];
-
+		
 		//print (currMouse.ToString () + " WHEEEE");
 	}
-
+	
 	private void addWitnesses(){
-
+		
 		foreach (NPC n in witnessList) {
-
+			
 			idealGameState.addNewEntry(new DictEntry(n.getEnumName(), GuiltLevel.witness,
 			                                         n.getWeaponProf(), n.getAlibi(), n.getTrust()));
-
+			
 		}
-
+		
 	}
-
+	
 	//Testing purposes
 	void Start(){
 		//start location
@@ -267,12 +268,11 @@ public class GameManager : MonoBehaviour {
 		nextY = -5.890769f;
 		//For a changing cursor, load in all of its sprites into the list
 		setIcons ();
-
+		
 		// Used for Dialoguer components
-		Debug.Log ("Persocets, Adderall, Ecstasy, PMW");
 		DialogueGUI dGUI = gameObject.AddComponent<DialogueGUI> ();
 		dGUI.setSkin(Resources.Load ("OldSchool") as GUISkin);
-
+		
 		roomIDList = new ArrayList ();
 		rooms = new string[5];
 		rooms[2] = "finoffice";
@@ -301,10 +301,10 @@ public class GameManager : MonoBehaviour {
 		//generateCase ();
 		theCase = generator.demo ();
 		addWitnesses ();
-
+		
 		printGoal ();
 	}
-
+	
 	public static List<NPC> getSceneNPCList(int sceneID){ 
 		List<NPC> temp = new List<NPC>();
 		foreach (NPC n in npcList) {
@@ -314,10 +314,10 @@ public class GameManager : MonoBehaviour {
 				//Debug.Log("Match found");
 				temp.Add(n);
 			}
-				}
+		}
 		return temp;
 	}
-
+	
 	public static List<NPC> getSceneWitnessList(int sceneID){ 
 		List<NPC> temp = new List<NPC>();
 		foreach (NPC n in witnessList) {
@@ -328,11 +328,11 @@ public class GameManager : MonoBehaviour {
 		}
 		return temp;
 	}
-
+	
 	public void finishEpisode(){
 		currentEpisode++;
 	}
-
+	
 }
 
 public enum gameStates {
